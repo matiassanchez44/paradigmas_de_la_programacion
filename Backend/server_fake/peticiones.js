2b65d2e6bf457206f0bf828d87ab67a03ade8b44
matiasancho444-byte <matiassancho444@gmail.com>
2026-04-08 16:00:26 -0500

    talleres

---
+++ Backend/server_fake/peticiones.js
@@ -0,0 +1,145 @@
+var settings = {
+  "url": "http://localhost:3000/privados/TZ5AoUBLWyQ",
+  "method": "GET",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/9FYzKPOqyRY",
+  "method": "GET",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/yuFCuz8tmf0",
+  "method": "GET",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/WlX_E0Wqatc",
+  "method": "GET",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/",
+  "method": "POST",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "Nombre completo": "Jose devora limoncitas",
+    "Apellidos Completos": "Roldan",
+    "Edad": 81,
+    "Correo Electronico": "bcolbeck6@umich.edu",
+    "Fecha de Nacimiento": "9/20/1965",
+    "Ciudad": "Rakvere",
+    "Id": "68.56.32.196/23",
+    "id": "miZPtSKuNp8"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/NSPNSfIl0N8",
+  "method": "DELETE",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/miZPtSKuNp8",
+  "method": "DELETE",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/GGCgQ5nYRLw",
+  "method": "DELETE",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/TZ5AoUBLWyQ",
+  "method": "PUT",
+  "timeout": 0,
+  "headers": {
+    "Content-Type": "application/json"
+  },
+  "data": JSON.stringify({
+    "Nombre completo": "Elmore Pepye",
+    "Apellidos Completos": "Pepye",
+    "Edad": 10,
+    "Correo Electronico": "epepye0@ucoz.ru",
+    "Fecha de Nacimiento": "6/19/1997",
+    "Ciudad": "",
+    "Id": "130.184.158.178/10",
+    "id": "TZ5AoUBLWyQ"
+  }),
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
+
+
+
+
+var settings = {
+  "url": "http://localhost:3000/privados/WlX_E0Wqatc",
+  "method": "GET",
+  "timeout": 0,
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
--- Backend/server_fake/readme.md
+++ Backend/server_fake/readme.md
@@ -4,3 +4,22 @@
 ¿Cuando hacer una peticion tipo PATCH?
 ¿Que es SOAP, cuales somn las diferencias con REST?
 ¿Que es GraphQL, cuales son las diferencias con REST?
+
+                   SOLUCION
+
+Indica que una solicitud fue exitosa y que como resultado se creó un nuevo recurso en el servido
+
+Básicamente vuelves a consultar el mismo recurso y revisas si los datos ya cambiaron. Si ves los cambios, entonces sí se actualizo
+
+200s: todo bien
+300s: redirecciones
+400s: error del cliente (algo hiciste mal en la petición)
+500s: error del servidor (fallo del backend)
+
+Cuando quieres cambiar solo una parte de algo, no todo el recurso completo
+
+SOAP es un protocolo más estricto que usa XML.
+REST es un estilo más flexible que usa HTTP y distintos formatos
+
+GraphQL te deja pedir exactamente lo que necesitas en una sola consulta.
+REST a veces te da datos de más o te obliga a hacer varias peticiones
